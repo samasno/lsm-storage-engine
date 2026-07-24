3a30328f1d6c9ee6a18a7f74da565c483a3df7cd
samasno <samasno@samasno.com>
2026-07-24 16:00:54 -0500

    seek lte on mock

--- db/db_writer_test.go
+++ db/db_writer_test.go
@@ -148,12 +148,20 @@ func (mm *mockMemtable) Seek([]byte) []byte {
 }
 
 func (mm *mockMemtable) SeekEqualOrLower(seekkey []byte) (key []byte, value []byte) {
-	value, ok := mm.data[string(seekkey)]
-	if !ok {
-		return nil, nil
+	mm.mtx.Lock()
+	defer mm.mtx.Unlock()
+
+	for k, v := range mm.data {
+		kb := []byte(k)
+		if bytes.Compare(kb, seekkey) <= 0 {
+			if key == nil || bytes.Compare(kb, key) > 0 {
+				key = kb
+				value = v
+			}
+		}
 	}
 
-	return seekkey, value
+	return key, value
 }
 
 func (mm *mockMemtable) Delete(key []byte) error {
@@ -164,7 +172,7 @@ func (mm *mockMemtable) Insert(key []byte, value []byte) {
 	mm.mtx.Lock()
 	defer mm.mtx.Unlock()
 	mm.data[string(key)] = value
-	return
+	// fmt.Printf("%v", key)
 }
 
 func assertEqual[T comparable](t *testing.T, name string, actual, expected T) {
