25b72173d880890a514d4425c5c62e5ec2030c54
samasno <samasno@samasno.com>
2026-07-29 12:18:00 -0500

    added storage interfaces

--- types/types.go
+++ types/types.go
@@ -1,5 +1,9 @@
 package dbtypes
 
+import (
+	"os"
+)
+
 type Memtable interface {
 	Insert(key []byte, value []byte)
 	Seek(seekkey []byte) (value []byte)
@@ -12,4 +16,17 @@ type Scanner interface {
 	Key() []byte
 	Value() []byte
 	Release() error
+}
+
+type Storage interface {
+	Files() []*os.File
+	NewFile(filename string) (StorageFile, error)
+}
+
+type StorageFile interface {
+	Write(data []byte) (int, error)
+	WriteAt(offset uint64, data []byte) error
+	Read(buf []byte) (int, error)
+	ReadAt(offset uint64, buf []byte) error
+	Seek(whence int) (int, error)
 }
