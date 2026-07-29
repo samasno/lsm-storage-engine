da9bc1c493beebea8f12d13dfa12d83f771d2f40
samasno <samasno@samasno.com>
2026-07-29 07:28:42 -0500

    add scanner to memtable interface

--- types/types.go
+++ types/types.go
@@ -4,6 +4,7 @@ type Memtable interface {
 	Insert(key []byte, value []byte)
 	Seek(seekkey []byte) (value []byte)
 	SeekEqualOrLower(seekkey []byte) (key []byte, value []byte)
+	Scanner(startKey []byte) Scanner
 }
 
 type Scanner interface {
